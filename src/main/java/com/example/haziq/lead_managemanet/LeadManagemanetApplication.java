926c8110add5609befa9e31569f20263e0aff04e
Muhammad Haziq <beinghaziq@gmail.com>
2023-07-06 13:54:59 +0500

    first commit

---
+++ src/main/java/com/example/haziq/lead_managemanet/LeadManagemanetApplication.java
@@ -0,0 +1,13 @@
+package com.example.haziq.lead_managemanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadManagemanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadManagemanetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/haziq/lead_managemanet/LeadManagemanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.haziq.lead_managemanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadManagemanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
