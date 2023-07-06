88c7a6d0dc58e708c31e0a38f5ba16e7be76c29b
Muhammad Haziq <beinghaziq@gmail.com>
2023-07-06 15:50:07 +0500

    fields added in user moded

--- src/main/java/com/example/haziq/lead_managemanet/models/User.java
+++ src/main/java/com/example/haziq/lead_managemanet/models/User.java
@@ -1,4 +1,25 @@
 package com.example.haziq.lead_managemanet.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity(name = "users")
 public class User {
+  @Id
+  @GeneratedValue
+  private Integer id;
+  @Column(unique = true)
+  @NotBlank(message = "Email is required")
+  @Email(message = "Invalid email format")
+  private String email;
+
+  @NotBlank(message = "Name is required")
+  private String full_name;
+
+  @NotBlank(message = "Password is required")
+  private String password;
 }
