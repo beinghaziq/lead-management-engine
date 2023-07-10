6af82ce287494029636da7f7c310093630893692
Muhammad Haziq <72576839+beinghaziq@users.noreply.github.com>
2023-07-10 19:45:36 +0500

    multi image support added in comments (#4)

--- src/main/java/com/example/haziq/lead_managemanet/models/Comment.java
+++ src/main/java/com/example/haziq/lead_managemanet/models/Comment.java
@@ -1,6 +1,9 @@
 package com.example.haziq.lead_managemanet.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -11,6 +14,10 @@ public class Comment {
   @Id
   @GeneratedValue
   private Integer id;
+  @NotBlank(message = "content is required")
+  private String content;
+  @OneToMany(mappedBy = "comment", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Image> images;
   @Column(updatable = false)
   @CreationTimestamp
   private LocalDateTime created_at;
