970e7c551d87a96c2609df4885f35bcf32f8400c
Muhammad Haziq <beinghaziq@gmail.com>
2023-07-10 18:38:14 +0500

    main class cleaned

--- src/main/java/com/example/haziq/lead_managemanet/LeadManagemanetApplication.java
+++ src/main/java/com/example/haziq/lead_managemanet/LeadManagemanetApplication.java
@@ -1,11 +1,7 @@
 package com.example.haziq.lead_managemanet;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class LeadManagemanetApplication {
@@ -13,14 +9,4 @@ public class LeadManagemanetApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LeadManagemanetApplication.class, args);
 	}
-  @Configuration
-  public class SecurityConfig {
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-      return new BCryptPasswordEncoder();
-    }
-
-  }
-
 }
