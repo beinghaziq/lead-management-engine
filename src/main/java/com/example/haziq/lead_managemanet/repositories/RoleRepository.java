f948814824e0c9ec3596def876f5efa85e03d7d6
Muhammad Haziq <beinghaziq@gmail.com>
2023-07-12 15:16:20 +0500

    data seeding added for roles

---
+++ src/main/java/com/example/haziq/lead_managemanet/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.haziq.lead_managemanet.repositories;
+
+import com.example.haziq.lead_managemanet.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
