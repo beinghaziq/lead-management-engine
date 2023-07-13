cf57d724097494b9703c10dea6a26592e52f7c9e
Muhammad Haziq <beinghaziq@gmail.com>
2023-07-13 17:36:19 +0500

    unused methods removed

--- src/main/java/com/example/haziq/lead_managemanet/repositories/UserRepository.java
+++ src/main/java/com/example/haziq/lead_managemanet/repositories/UserRepository.java
@@ -1,13 +1,6 @@
 package com.example.haziq.lead_managemanet.repositories;
 
-import com.example.haziq.lead_managemanet.enums.RoleName;
-import com.example.haziq.lead_managemanet.models.Role;
 import com.example.haziq.lead_managemanet.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-import static com.example.haziq.lead_managemanet.enums.RoleName.SuperAdmin;
-
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
