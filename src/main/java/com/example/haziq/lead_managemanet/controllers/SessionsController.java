c39c636bfa1c005d646b7661b5c6f2254020f6ae
Muhammad Haziq <beinghaziq@gmail.com>
2023-07-17 18:21:03 +0500

    basic logout handled

--- src/main/java/com/example/haziq/lead_managemanet/controllers/SessionsController.java
+++ src/main/java/com/example/haziq/lead_managemanet/controllers/SessionsController.java
@@ -2,11 +2,19 @@ package com.example.haziq.lead_managemanet.controllers;
 
 import com.example.haziq.lead_managemanet.models.User;
 import com.example.haziq.lead_managemanet.repositories.RoleRepository;
+import com.example.haziq.lead_managemanet.responses.MessageResponse;
 import com.example.haziq.lead_managemanet.services.AuthenticationService;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.UUID;
 
 public class SessionsController {
   private UserRepository repository;
@@ -18,20 +26,35 @@ public class SessionsController {
   }
   @PostMapping(path = "/login")
   public User create(@RequestBody User user, HttpServletResponse response) {
-    User user repository.findByEmail(user.getEmail());
+    User loggedUser = repository.findByEmail(user.getEmail());
     AuthenticationService service = new AuthenticationService(passwordEncoder);
-    user_repo
-    service.isValidPassword("123123123", );
+    loggedUser.setAuth_token(UUID.randomUUID().toString());
+    repository.save(loggedUser);
+    boolean is_valid = service.isValidPassword(user.getPassword(), loggedUser.getPassword());
 
-//    UserAdapter adapter = new UserAdapter(repository, role_repository, user, rollName);
-//    Cookie cookie = new Cookie("username", "Jovan");
-//    UserAdapter adapter = new UserAdapter(repository, role_repository, user, rollName, passwordEncoder);
-//    User created_user = adapter.create_user_with_role();
-//    Cookie cookie = new Cookie("HTTP-X-AUTH-TOKEN", created_user.getAuth_token());
-//    cookie.setHttpOnly(true);
-//    //add cookie to response
-//    response.addCookie(cookie);
-//    User created_user = adapter.create_user_with_role();
+    Cookie cookie = new Cookie("HTTP-X-AUTH-TOKEN", loggedUser.getAuth_token());
+    cookie.setHttpOnly(true);
+    response.addCookie(cookie);
     return user;
+  }
+
+  @DeleteMapping(path = "/logout")
+  public ResponseEntity<MessageResponse> destroy(@RequestParam String email, HttpServletResponse response) {
+    User loggedUser = repository.findByEmail(user.getEmail());
+    AuthenticationService service = new AuthenticationService(passwordEncoder);
+    loggedUser.setAuth_token(UUID.randomUUID().toString());
+    repository.save(loggedUser);
+    boolean is_valid = service.isValidPassword(user.getPassword(), loggedUser.getPassword());
+
+    Cookie cookie = new Cookie("HTTP-X-AUTH-TOKEN", null);
+    cookie.setMaxAge(0);
+    cookie.setSecure(true);
+    cookie.setHttpOnly(true);
+    cookie.setPath("/");
+
+//add cookie to response
+    response.addCookie(cookie);
+    MessageResponse message = new MessageResponse("User logged out successfully");
+    return ResponseEntity.status(HttpStatus.OK).body(message);
   }
 }
---
+++ src/main/java/com/example/haziq/lead_managemanet/responses/MessageResponse.java
@@ -0,0 +1,9 @@
+package com.example.haziq.lead_managemanet.responses;
+
+public class MessageResponse {
+  private String message;
+
+  public MessageResponse(String message) {
+    this.message = message;
+  }
+}
