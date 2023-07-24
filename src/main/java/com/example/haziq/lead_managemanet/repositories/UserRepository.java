af4b75c63852e43370942d719a8f9aec99793921
Muhammad Haziq <72576839+beinghaziq@users.noreply.github.com>
2023-07-24 16:15:55 +0500

    not found exception handled (#11)

--- src/main/java/com/example/haziq/lead_managemanet/repositories/UserRepository.java
+++ src/main/java/com/example/haziq/lead_managemanet/repositories/UserRepository.java
@@ -2,6 +2,13 @@ package com.example.haziq.lead_managemanet.repositories;
 
 import com.example.haziq.lead_managemanet.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
   User findByEmail(String email);
+  @Query("SELECT u FROM users u WHERE u.auth_token = :authToken")
+  User findByAuthToken(@Param("authToken") String authToken);
 }
