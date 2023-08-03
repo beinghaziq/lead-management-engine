4c908fb1088e2d384f33ae4e03939b8bb9335706
Muhammad Haziq <72576839+beinghaziq@users.noreply.github.com>
2023-08-03 15:54:56 +0500

    authorization added on create leads (#14)

--- src/main/java/com/example/haziq/lead_managemanet/LeadManagemanetApplication.java
+++ src/main/java/com/example/haziq/lead_managemanet/LeadManagemanetApplication.java
@@ -2,8 +2,11 @@ package com.example.haziq.lead_managemanet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+
 public class LeadManagemanetApplication {
 
 	public static void main(String[] args) {
